364ae9f8ecb676a778f85a17fb7e7bcda30ec2d7
Nach0rr0 <ignacio.roman@live.cl>
2024-04-08 13:25:27 -0400

    first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userCollection = "users";
+
+const UserSchema = new mongoose.Schema({
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(this.password, salt);
+      this.password = hashedPassword;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next();
+  }
+});
+
+const User = mongoose.model(userCollection, UserSchema);
+
+export default User;
